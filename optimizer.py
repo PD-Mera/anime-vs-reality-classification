c066cd7e03a62deebb6aa824d1f018f115093292
Phương Đông <82107107+PD-Mera@users.noreply.github.com>
2023-02-14 15:19:20 +0700

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,14 +1,6 @@
 import torch
 
 
-class ClsfLoss():
-    def __init__(self):
-        super(ClsfLoss, self).__init__()
-        pass
-    def forward(self, x):
-        return x
-
-
 def init_optimizer(model, config):
     assert config['train']['optimizer'] in config['OPTIMIZER_AVAILABLE'], f'"optimizer" in `config.py` must in {config["OPTIMIZER_AVAILABLE"]}'
     if config['train']['optimizer'] == 'Adam':
